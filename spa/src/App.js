5e8ad83028e964a9c00a9cba47c78bb594b32160
Felipe Faé Schwade <felipefaeschwade@hotmail.com>
2019-08-27 16:59:30 -0400

    TEste 6

--- spa/src/App.js
+++ spa/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Lindomar outro teste AO VIVO.
+          Edit <code>src/App.js</code> and Lindomar outro teste AO VIVASSO.
         </p>
         <a
           className="App-link"
