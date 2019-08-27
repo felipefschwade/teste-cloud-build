ca1d6f0675a89da5f926af42364f8b243d7fb3e6
Felipe Faé Schwade <felipefaeschwade@hotmail.com>
2019-08-27 18:13:19 -0400

    Teste api 1.

--- api/src/controllers/helloWorldController.ts
+++ api/src/controllers/helloWorldController.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 export default class HellowWordController {
   public static async hello(req: express.Request, res: express.Response): Promise<void> {
     res.status(200).send({
-      message: 'GET request successfulll!!!!'
+      message: 'GET deu certo meu CÃO SAGRADO'
     })
   }
 }
